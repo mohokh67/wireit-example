beea9566e604bdd228489dc7b6a15ea2aca4d5c4
MoHo Khaleqi <moho.khaleqi@checkout.com>
2022-11-18 13:12:06 +0000

    express app before using wireit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, { Express, Request, Response } from 'express';
+import pino from 'pino-http'
+import { createUser } from './utils/helper'
+
+const logger = pino()
+
+
+
+const app: Express = express();
+const port = 8000;
+
+app.use(logger)
+
+app.get('/ping', (req: Request, res: Response) => {
+  logger(req, res);
+
+  req.log.info('GET /ping was called')
+  res.send('pong 2');
+});
+
+app.post('/user', async(req: Request, res: Response) => {
+  logger(req, res)
+  req.log.info('POST /user was called')
+
+  await createUser()
+
+  res
+    .status(201)
+    .send('user created');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ src/utils/helper.ts
@@ -0,0 +1,14 @@
+import pino from 'pino'
+
+const logger = pino();
+
+// const x= 'xx'
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+export const createUser = async () :Promise<boolean> => {
+  logger.info('creating user!');
+  await sleep(2000);
+  logger.info('user created')
+  return true
+}
