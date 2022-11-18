d92d45c7209017cb1d6ddbabe431c30b967b8dfa
MoHo Khaleqi <moho.khaleqi@checkout.com>
2022-11-18 13:45:20 +0000

    feat: add wireit

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,26 @@ import { createUser } from './utils/helper'
 
 const logger = pino()
 
-
-
 const app: Express = express();
 const port = 8000;
 
 app.use(logger)
 
 app.get('/ping', (req: Request, res: Response) => {
-  logger(req, res);
+  // logger(req, res);
 
   req.log.info('GET /ping was called')
-  res.send('pong 2');
+
+  res.send('pong!');
 });
 
 app.post('/user', async(req: Request, res: Response) => {
-  logger(req, res)
+  // logger(req, res)
   req.log.info('POST /user was called')
-
   await createUser()
 
-  res
-    .status(201)
-    .send('user created');
+  res.status(201).send('user created');
 });
-
-
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
